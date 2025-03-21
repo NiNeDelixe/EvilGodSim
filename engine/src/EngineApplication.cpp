411ba360dd3c5adc2daa9498aa208c2eb76ae002
NiNeDelixe <alkor.sidius@gmail.com>
2025-03-21 14:24:15 +0300

    Добавьте файлы проекта.

---
+++ engine/src/EngineApplication.cpp
@@ -0,0 +1,13 @@
+﻿#include "EngineApplication.h"
+
+EngineApplication* EngineApplication::m_instance = nullptr;
+
+EngineApplication::EngineApplication(int argc, char** argv)
+{
+	EngineApplication::m_instance = this;
+}
+
+int EngineApplication::exec() noexcept
+{
+	return 0;
+}
---
+++ game/src/main.cpp
@@ -0,0 +1,8 @@
+﻿#include <EngineApplication.h>
+
+int main(int argc, char** argv)
+{
+	EngineApplication app(argc, argv);
+	
+	return app.exec();
+}
