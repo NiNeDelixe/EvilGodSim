e0d14ca211561372a8dad296a1e0fee37e3e85cb
NiNeDelixe <alkor.sidius@gmail.com>
2026-02-02 18:11:09 +0300

    submodule working

--- game/src/base/GameApplication.cpp
+++ game/src/base/GameApplication.cpp
@@ -14,24 +14,9 @@ GameApplication::~GameApplication()
 void GameApplication::prefix()
 {
 	EngineCoreApplication::prefix();
-
-	//debug
-	{
-		if (m_debug_window)
-		{
-			ImGui::ShowMetricsWindow();
-
-		}
-	}
 }
 
 void GameApplication::postfix()
 {
 	EngineCoreApplication::postfix();
-
-}
-
-void GameApplication::debugButtonWindowPressed(InputType type, int key, int scancode, int action, int mods)
-{
-	m_debug_window = !m_debug_window;
 }
