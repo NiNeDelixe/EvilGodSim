2bad3ded35e29c61be95a926991e4daf132e9291
NiNeDelixe <alkor.sidius@gmail.com>
2026-02-24 00:31:10 +0300

    clean up ecs manager

--- engine/include/enginecore/core/ecs/systems/managment/SystemsManager.h
+++ engine/include/enginecore/core/ecs/systems/managment/SystemsManager.h
@@ -5,7 +5,7 @@
 #include "enginecore/core/CoreDefines.h"
 
 #include <atomic>
-#include <unordered_map>
+#include <shared_mutex>
 
 #include "enginecore/core/LibrariesDefines.h"
 
@@ -24,9 +24,9 @@ class SystemsManager
 {
 public:
     SystemsManager(int max_concurrency = std::thread::hardware_concurrency()) 
-        : m_arena(max_concurrency) 
+        : m_arena(max_concurrency > 0 ? max_concurrency : 1) 
         {}
-    ~SystemsManager() {}
+    ~SystemsManager() = default;
 
 public:
     void scheduledSystem(EntityRegistry<DefaultEntityIndentifier>& registry)
@@ -35,65 +35,61 @@ public:
         {
             if (!m_systems.getUncategorized().empty())
             {
-                ENGTHRDPL::parallel_for(
-                    ENGTHRDPL::blocked_range<size_t>(0, m_systems.getUncategorized().size()),
-                    [&](const ENGTHRDPL::blocked_range<size_t>& range) 
-                    {
-                        for (size_t i = range.begin(); i < range.end(); ++i)
-                        {
-                            auto& system = m_systems[i];
-
-                            m_active_tasks.fetch_add(1, std::memory_order_relaxed);
-                            system->update(registry);
-                            m_active_tasks.fetch_sub(1, std::memory_order_relaxed);
-                        }
-                    }
+                parallelUpdate(
+                    m_systems.getUncategorized().begin(),
+                    m_systems.getUncategorized().end(),
+                    registry
                 );
             }
             
             if (!m_systems.getCategorized().empty())
             {
-                ENGTHRDPL::parallel_for_each(
-                    m_systems.getCategorized().begin(), m_systems.getCategorized().end(),
-                    [&](const auto& pair) 
-                    {
-                        auto& system = pair.second;
-
-                        m_active_tasks.fetch_add(1, std::memory_order_relaxed);
-                        system->update(registry);
-                        m_active_tasks.fetch_sub(1, std::memory_order_relaxed);
-                    }
+                parallelUpdate(
+                    m_systems.getCategorized().begin(),
+                    m_systems.getCategorized().end(),
+                    registry
                 );
             }
             
         });
     }
 
-    // void addSystem(const SystemCategory& type, const std::shared_ptr<ISystem>& system) 
-    // { 
-    //     auto it = m_categories.find(type);
-        
-    //     if (it != m_categories.end()) 
-    //     {
-    //         size_t index = std::distance(m_categories.begin(), it);
-    //         m_systems[index] = system;
-    //     }
-    //     else
-    //     {
-    //         m_systems.push_back(system);
-    //         m_categories.insert(type); 
-    //     }
-    // }
+    template<typename ITERATOR>
+    void parallelUpdate(ITERATOR begin,
+                        ITERATOR end,
+                        EntityRegistry<DefaultEntityIndentifier>& registry)
+    {
+        ENGTHRDPL::parallel_for_each(
+            begin, end,
+            [&](auto& element)
+            {
+                auto&& system = extractSystem(element);
+                m_active_tasks.fetch_add(1, std::memory_order_relaxed);
+                system->update(registry);
+                m_active_tasks.fetch_sub(1, std::memory_order_relaxed);
+            }
+        );
+    }
 
-    // void addSystem(const std::shared_ptr<ISystem>& system) 
-    // { 
-    //     m_systems.push_back(system);
-    // }
+    static ISystem* extractSystem(auto& element)
+    {
+        if constexpr (std::is_same_v<
+            std::decay_t<decltype(element)>,
+            std::shared_ptr<ISystem>>)
+        {
+            return element.get();
+        }
+        else
+        {
+            return element.second.get();
+        }
+    }
 
     template<CONCEPTS_NAMESPACE::IsDerivedFromISystemFactory SYSTEMFACTORY, class... ARGS>
     void emplaceSystems(ARGS&&... args) 
     {
-        m_systems = SYSTEMFACTORY(std::forward<decltype(args)>(args)...).createSystems();
+        SYSTEMFACTORY factory(std::forward<ARGS>(args)...);
+        m_systems = factory.createSystems();
     }
 
 public:
@@ -102,8 +98,9 @@ public:
     SETTER(SystemsContainer, m_systems, Systems)
 
 private:
+    mutable std::shared_mutex m_systems_mutex;
+
     SystemsContainer m_systems;
-    //std::set<SystemCategory> m_categories;
     tbb::task_arena m_arena;
     std::atomic<size_t> m_active_tasks{0};
 };
